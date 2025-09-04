d95bbec1603eb1f65e05ab019a15858c11eedc47
riric0602 <chbouki.riham@gmail.com>
2025-09-04 12:00:58 +0000

    p1: k3s for client is SET
    Signed-off-by: riric0602 <chbouki.riham@gmail.com>

--- p1_riri/scripts/server.sh
+++ p1_riri/scripts/server.sh
@@ -3,9 +3,14 @@ set -euo pipefail
 
 echo "===== Installing K3s in Controller Mode on the server VM ====="
 
+PRIVATE_IP="192.168.56.110"
+
 # --- Install K3s ---
 echo ">>> Installing K3s..."
 curl -sfL https://get.k3s.io | sh -
+echo ">>> Starting K3s..."
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - \
+    server --node-ip "$PRIVATE_IP" --advertise-address "$PRIVATE_IP" --tls-san "$PRIVATE_IP"
 
 # --- Configure kubeconfig for current user ---
 echo ">>> Configuring kubectl..."
@@ -14,7 +19,7 @@ sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
 
 # --- Save node token to .env ---
-NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token | cut -d':' -f1)
+NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 SHARED_FOLDER="/home/vagrant/p1"
 echo "$NODE_TOKEN" > "$SHARED_FOLDER/tokens/node"
 echo ">>> Node token saved to $SHARED_FOLDER/tokens/node"
--- p1_riri/scripts/worker.sh
+++ p1_riri/scripts/worker.sh
@@ -6,16 +6,13 @@ echo "===== Installing K3s in Agent Mode on the worker VM ====="
 # --- Read server IP and node token from shared folder ---
 SHARED_FOLDER="/home/vagrant/p1"
 SERVER_IP="192.168.56.110"
-NODE_TOKEN=$(< "$SHARED_FOLDER/tokens/node")
+K3S_TOKEN=$(< "$SHARED_FOLDER/tokens/node")
 
-echo ">>> Using Node Token from $NODE_TOKEN"
+echo ">>> Using Node Token from $K3S_TOKEN"
 echo ">>> Connecting to K3s server at $SERVER_IP"
 
 echo ">>> Installing K3s agent (non-blocking)..."
-curl -sfL https://get.k3s.io | \
-    K3S_URL="https://$SERVER_IP:6443" \
-    K3S_TOKEN="$NODE_TOKEN" \
-    K3S_AGENT_SKIP_START=true sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$K3S_TOKEN sh -
 
 echo ">>> Starting K3s agent service..."
 sudo systemctl enable k3s-agent
