293df873273858e0671ebb6e5d85ab08bc1a8723
riric0602 <chbouki.riham@gmail.com>
2025-09-08 11:02:00 +0000

    p3: getting there
    Signed-off-by: riric0602 <chbouki.riham@gmail.com>

--- p3/scripts/setup_temp.sh
+++ p3/scripts/setup_temp.sh
@@ -5,7 +5,7 @@ k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2
 kubectl cluster-info
 
 # install argo cd (maybe get newer version)
-wget https://github.com/argoproj/argo-cd/raw/v1.6.2/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl create namespace argocd
 kubectl apply -f install.yaml -n argocd
@@ -13,7 +13,9 @@ kubectl apply -f install.yaml -n argocd
 # install argo crds
 kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
 
-# add --insecure and --rootpath and - /argocd in install.yaml after  –staticassets 
+# check all pods are running right > if false do not move on
+kubectl get pods -n argocd
 
 # create ingress.yml
 kubectl apply -f ingress.yml -n argocd
+
